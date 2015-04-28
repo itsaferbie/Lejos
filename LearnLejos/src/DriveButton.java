bd33afa01be0785fd2924fc896bf25f680f94a4e
Donald Rohlman III <itsaferbie@gmail.com>
2015-04-28 10:01:33 -0600

    Adding some first files to repo

---
+++ LearnLejos/src/DriveButton.java
@@ -0,0 +1,12 @@
+
+public class DriveButton {
+
+	public static void main(String[] args) {
+		/*try {
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+	}
+
+}
---
+++ LearnLejos/src/Move2Sec.java
@@ -0,0 +1,22 @@
+import lejos.nxt.Motor;
+
+public class Move2Sec {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Moving forward...");
+			Motor.A.forward();
+			Motor.B.forward();
+			Thread.sleep(1000);
+			System.out.println("Moving backward...");
+			Motor.A.backward();
+			Motor.B.backward();
+			Thread.sleep(2000);
+			Motor.A.stop();
+			Motor.B.stop();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
